bc727d5671a2f50ec23252afd8d81c2d3e08b542
XIAOYixuan <xiaoyx.bit@gmail.com>
2021-12-17 23:37:37 +0100

    new feat: graph test ref #4

--- research_trends/ver0/urls.py
+++ research_trends/ver0/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 app_name = "ver0"
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("keywords", views.keywords_page, name="keywords"),
+    path("researchers", views.researchers_page, name="researchers"),
+    path("institues", views.institutes_page, name="institutes")
 ]
--- research_trends/ver0/views.py
+++ research_trends/ver0/views.py
@@ -6,3 +6,12 @@ from django.http.response import HttpResponseRedirect
 # Create your views here.
 def index(request):
     return render(request, "ver0/index.html")
+
+def keywords_page(request):
+    return render(request, "ver0/keywords.html")
+
+def researchers_page(request):
+    return render(request, "ver0/researchers.html")
+
+def institutes_page(request):
+    return render(request, "ver0/institutes.html")
