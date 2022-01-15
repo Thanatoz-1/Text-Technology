1f94eefeffc0d7ad9d4f1726112ce010e8d711c3
Tushar Dhyani <dhyanitushar@gmail.com>
2022-01-15 12:44:16 +0100

    Updated logs

--- api/mod/api/v1/views.py
+++ api/mod/api/v1/views.py
@@ -6,8 +6,9 @@ from api import app, db
 from flask import Blueprint
 from datetime import datetime
 from api.mod.packages.prettify import termcolor
+from bson.json_util import dumps
 
-collection = db.get_collection("TextTechnology")
+collection = db["TextTechnology"]
 
 logging.info(
     f"{termcolor.HEADER}Application running on {datetime.now().strftime('%Y-%m-%d::%H:%M')}{termcolor.ENDC}"
@@ -20,6 +21,7 @@ blueprint_agent = Blueprint(
 
 @blueprint_agent.route("/")
 def hello():
+    logging.debug("Call from the root url")
     return "<h1 style='color:blue'>Text Technology API!</h1>"
 
 
@@ -28,11 +30,23 @@ def query_example():
     # if key doesn't exist, returns None
     key = request.args.get("query")
     value = request.args.get("target")
+    print(key, value)
     query = {key: {"$regex": value, "$options": "i"}}
-    print(f"query ready {query}")
+    logging.debug(f"query ready {query}")
     results = collection.find_one(query)
+    # return True
+
     return f"""
     <h4>{results["title"]} </h4>
     <p>{results["author"]} </p>
     <p>{results["abstract"]} </p>
     """
+
+
+@blueprint_agent.route("/nash")
+def keyword_year():
+    key = request.args.get("keyword")
+    y = request.args.get("year")
+    var = {key: {"$regex": y}}
+    result = collection.find(var)
+    return result
