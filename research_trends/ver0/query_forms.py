567abd6e127ac40bded0028b13537cf03ee36821
XIAOYixuan <xiaoyx.bit@gmail.com>
2022-02-24 16:26:36 +0100

    enhance: input validation #27

--- research_trends/ver0/query_forms.py
+++ research_trends/ver0/query_forms.py
@@ -9,7 +9,7 @@ class KeywordsFilterForm(forms.Form):
           trend
     st_year, ed_year: the start year and end year of the trend
     """
-    topk = forms.IntegerField(label="topk", initial=5)
+    topk = forms.IntegerField(label="topk", min_value=1, max_value=50, initial=5)
     keywords = forms.CharField(label="keywords", initial='x')
     st_year = forms.IntegerField(label="start year", min_value=2010, max_value=2021, initial=2015)
     ed_year = forms.IntegerField(label="end year", min_value=2010, max_value=2021, initial=2020)
@@ -19,7 +19,7 @@ class ResearchFilterForm(forms.Form):
     topk: only present top k fields of interest
     author: the name of one author
     """
-    topk = forms.IntegerField(widget=forms.NumberInput(attrs={'placeholder': 'topK', 'style': 'width: 300px;', 'class': 'form-control'}))
+    topk = forms.IntegerField(min_value=1, max_value=50, widget=forms.NumberInput(attrs={'placeholder': 'topK', 'style': 'width: 300px;', 'class': 'form-control'}))
     author = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'author', 'style': 'width: 300px;', 'class': 'form-control'}))
 
 class AffiliationFilterForm(forms.Form):
@@ -27,5 +27,5 @@ class AffiliationFilterForm(forms.Form):
     topk: only present top k fields of interest
     affiliation: the name of one affiliation
     """
-    topk = forms.IntegerField(widget=forms.NumberInput(attrs={'placeholder': 'topK', 'style': 'width: 300px;', 'class': 'form-control'}))
+    topk = forms.IntegerField(min_value=1, max_value=50, widget=forms.NumberInput(attrs={'placeholder': 'topK', 'style': 'width: 300px;', 'class': 'form-control'}))
     affiliation = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'affiliation', 'style': 'width: 300px;', 'class': 'form-control'}))
